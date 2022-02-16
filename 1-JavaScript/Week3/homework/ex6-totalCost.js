0b7969b10d3f5e9795f5ed84e13317943c327eff
tonyclem <tonyclem001@gmail.com>
2022-02-17 00:12:25 +0100

    added ex6 and ex7 including testing

--- 1-JavaScript/Week3/homework/ex6-totalCost.js
+++ 1-JavaScript/Week3/homework/ex6-totalCost.js
@@ -21,22 +21,31 @@ instead!
 3. Complete the unit test functions and verify that all is working as expected.
 -----------------------------------------------------------------------------*/
 const cartForParty = {
-  // TODO complete this object
+  water: '1.3',
+  drink: '1.4',
+  chips: '2.4',
+  apple: '1.75',
+  orange: '0.99',
 };
 
-function calculateTotalPrice(/* TODO parameter(s) go here */) {
-  // TODO replace this comment with your code
+function calculateTotalPrice(myObject) {
+  let total = 0;
+  for (const [key, obj] of Object.entries(myObject)) {
+    total += parseFloat(`${obj}`);
+  }
+  return `Total: €${total}`;
 }
 
 // ! Test functions (plain vanilla JavaScript)
 function test1() {
   console.log('\nTest 1: calculateTotalPrice should take one parameter');
-  // TODO replace this comment with your code
+
+  console.log(calculateTotalPrice(cartForParty));
 }
 
 function test2() {
   console.log('\nTest 2: return correct output when passed cartForParty');
-  // TODO replace this comment with your code
+  console.log(calculateTotalPrice(cartForParty));
 }
 
 function test() {
